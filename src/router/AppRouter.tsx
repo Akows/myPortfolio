09c2310e996fa439dbfac4fb210d7a97e0e5346d
AKOWSPC <akows141@gmail.com>
2023-10-24 10:00:10 +0900

    Design: 구성된 컴포넌트 구조에 맞춰 approuter 구성을 완료, 동작을 확인함.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 import Home from '../pages/Home';
+import IntroduceMyself from '../pages/IntroduceMyself';
+import MyPortfolio from '../pages/MyPortfolio';
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" Component={Home} />
+        <Route path="/" element={<Home />} />
+        <Route path="/introducemyself" element={<IntroduceMyself />} />
+        <Route path="/myportfolio" element={<MyPortfolio />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
