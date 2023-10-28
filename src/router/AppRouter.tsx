900c4812aa2a6814f18853eb8f35c8129d396b27
AKOWSPC <akows141@gmail.com>
2023-10-28 16:35:09 +0900

    Feat: 매 페이지 렌더링 시마다 스크롤 위치를 페이지 최상단으로 이동시키는 기능을 구현.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
@@ -36,11 +36,25 @@ const themes = {
   },
 };
 
+// 페이지 렌더링 시 스크롤 위치를 페이지 최상단으로 이동시키는 역할의 컴포넌트.
+// useLocation 훅이 <Router> 컴포넌트의 컨텍스트 내에서만 사용되어야하기에 별도의 컴포넌트를 생성하여 기능을 처리한다.
+const ScrollToTop: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]); // 의존성 배열에 location.pathname을 넣어 라우트가 변경될 때마다 이 효과를 실행합니다.
+
+  // 이 컴포넌트는 JSX 컴포넌트지만 렌더링할 요소가 없다.
+  return null; // 아무것도 렌더링하지 않는 컴포넌트
+};
+
 const AppRouter: React.FC = () => {
   const currentTheme = useSelector((state: RootState) => state.theme.value);
 
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <ThemeProvider theme={themes[currentTheme]}>
         <Navbar />
         <Routes>
